dc24fced8c91c630ebbb32654bda4c2206dd0677
CHRIS <chris.shandgost@gmail.com>
2017-08-09 15:57:08 +1000

    Begin bitmask implementation for permission checking

--- discord/commands/command.go
+++ discord/commands/command.go
@@ -1,6 +1,10 @@
 package cmd
 
-import "github.com/bwmarrin/discordgo"
+import (
+	"container/list"
+
+	"github.com/bwmarrin/discordgo"
+)
 
 // Command works as an interface for other commands
 type Command interface {
@@ -9,11 +13,32 @@ type Command interface {
 
 // CommandCommon represents fields common between all commands
 type CommandCommon struct {
-	Caller      string
-	Response    string
-	Description string
+	Caller              string
+	Response            string
+	Description         string
+	Channels            *list.List
+	RequiredPermissions int
+	RequiredUsers       *list.List
 }
 
-func init() {
+const (
+	f0 = iota
+	f1 = iota
+	f2 = iota
+	//etc
+)
 
+func canExecute(s *discordgo.Session, m *discordgo.MessageCreate) (canExecute bool) {
+	userPerms, _ := s.UserChannelPermissions(m.Author.ID, m.ChannelID)
+	for i := 0; i < 32; i++ {
+		if RequiredPermissions&(1<<i) != 0 {
+			if userPerms & (0 << i) {
+				return false
+
+			}
+		}
+
+	}
+
+	return true
 }
